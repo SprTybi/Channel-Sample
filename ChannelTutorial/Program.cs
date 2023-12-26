216c7b4bda21543258c28a156f7e7a479feb530b
Sprtybi <Sprtybi@gmail.com>
2023-12-26 16:14:14 +0330

    Change Project Type to Worker Service

---
+++ ChannelTutorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using ChannelTutorial;
+using System.Threading.Channels;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+
+        services.AddHostedService<ReaderOneBackgroundService>();
+        services.AddSingleton(Channel.CreateBounded<string>(new BoundedChannelOptions(2)
+        {
+            Capacity = 2,
+            FullMode = BoundedChannelFullMode.Wait,  //رفتار پرودیوسر وقتی میبینه کیو پر هست
+            SingleReader = false,// میتونه ان تا ساب اسکریبر مسیج رو بخونه
+            SingleWriter = false, //
+
+        }, (string item) =>
+        { // وقتی اینجا معلوم میکنیم که استرینگه دیگر لازم نیست اول متد کریت باندد مشخص کنیم که جنس پیام های داخل صف از استرینگه
+            Console.WriteLine($"Droped Item : {item}");
+        }));
+    }).Build();
+
+
+host.Run();
---
+++ ChannelTutorial/ReaderOneBackgroundService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChannelTutorial
+{
+    public class ReaderOneBackgroundService : BackgroundService
+    {
+        private readonly Channel<string> _channel;
+        public ReaderOneBackgroundService(Channel<string> channel)
+        {
+            _channel = channel;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await foreach (var item in _channel.Reader.ReadAllAsync())
+                {
+                    Console.WriteLine($"Message Readddddddddddd: {item}");
+                }
+            }
+        }
+    }
+}
