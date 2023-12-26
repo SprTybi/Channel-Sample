488a10cd70b06fd4b2f44172890eb2fdcd91a2b3
Sprtybi <Sprtybi@gmail.com>
2023-12-26 14:35:14 +0330

    More Costomized With Description

--- Produser/Program.cs
+++ Produser/Program.cs
@@ -4,13 +4,22 @@ using System.Threading.Channels;
 Console.WriteLine("Start App");
 
 
-var channel = Channel.CreateBounded<string>(20);
+var channel = Channel.CreateBounded<string>(new BoundedChannelOptions(2)
+{
+    Capacity = 2,
+    FullMode = BoundedChannelFullMode.Wait,  //رفتار پرودیوسر وقتی میبینه کیو پر هست
+    SingleReader = false,// میتونه ان تا ساب اسکریبر مسیج رو بخونه
+    SingleWriter = false, //
+
+}, (string item) => { // وقتی اینجا معلوم میکنیم که استرینگه دیگر لازم نیست اول متد کریت باندد مشخص کنیم که جنس پیام های داخل صف از استرینگه
+    Console.WriteLine($"Droped Item : {item}");
+});
 var writer = Task.Run(async () =>
 {
     for (int i = 0; i < 50; i++)
     {
         await channel.Writer.WriteAsync(i.ToString());
-        Console.WriteLine($"Send Message : {i}");
+        Console.WriteLine($"Insert Message To QUEUE : {i}");
     }
 });
 
@@ -18,7 +27,7 @@ var reader = Task.Run(async () =>
 {
     await foreach (var item in channel.Reader.ReadAllAsync())
     {
-        Console.WriteLine($"Message Reqad {item}");
+        Console.WriteLine($"Message Readddddddddddd: {item}");
     }
 });
 
