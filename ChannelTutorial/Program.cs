645b6b405ba5c7f08819d82543c1cedce77b57c4
Sprtybi <Sprtybi@gmail.com>
2023-12-27 14:45:38 +0330

    Edit and Work Currently

--- ChannelTutorial/Program.cs
+++ ChannelTutorial/Program.cs
@@ -8,16 +8,27 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddHostedService<ReaderOneBackgroundService>();
         services.AddSingleton(Channel.CreateBounded<string>(new BoundedChannelOptions(2)
         {
-            Capacity = 2,
+            //Capacity = 2, // ظرفیت صف . در اینجا تعریف میکنیم یا در خط بالا در کانستراکتور اپشن مقدار دهس میکنیم
             FullMode = BoundedChannelFullMode.Wait,  //رفتار پرودیوسر وقتی میبینه کیو پر هست
-            SingleReader = false,// میتونه ان تا ساب اسکریبر مسیج رو بخونه
-            SingleWriter = false, //
+            //SingleReader = false,// میتونه ان تا ساب اسکریبر مسیج رو بخونه
+            //SingleWriter = false, //
 
         }, (string item) =>
         { // وقتی اینجا معلوم میکنیم که استرینگه دیگر لازم نیست اول متد کریت باندد مشخص کنیم که جنس پیام های داخل صف از استرینگه
             Console.WriteLine($"Droped Item : {item}");
         }));
     }).Build();
+
+var channel = host.Services.GetService<Channel<string>>();
+
+await Task.Run(async () => 
+{
+    for (int i = 0; i < 100; i++)
+    {
+        await channel.Writer.WriteAsync(i.ToString());
+        Console.WriteLine($"Send toooooo Queue{i}");
+    }
+});
 
 
 host.Run();
--- ChannelTutorial/ReaderOneBackgroundService.cs
+++ ChannelTutorial/ReaderOneBackgroundService.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
+﻿using System.Threading.Channels;
 
 namespace ChannelTutorial
 {
